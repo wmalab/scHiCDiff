3afdde2557ca007dd518f14827650d166e4506c1
lhlruc <36868570+lhlruc@users.noreply.github.com>
2023-03-26 23:11:43 -0700

    Add files via upload

---
+++ scHiCDiff.CVM.R
@@ -0,0 +1,33 @@
+scHiCDiff.CVM <-
+function(count.table,group){
+    library(twosamples)
+    binpair <- count.table[,c(1,2)]
+    counts <- count.table[,-c(1,2)]
+    row.names(counts) <- 1:nrow(counts)
+    binpairNum <- nrow(counts)
+
+    callDCI.CVM <- function(i){
+
+        counts_1 <- count.table[i,group == levels(group)[1]]
+        counts_2 <- count.table[i,group == levels(group)[2]]
+        results_binpair <- data.frame(row.names = row.names(counts)[i], stat = NA, pvalue = NA, pvalue.adj = NA)
+        res.binpair <- cvm_test(counts_1,counts_2)
+        results_binpair[1,"stat"] <- res.binpair[1]
+        results_binpair[1,"pvalue"] <- res.binpair[2]
+
+        return(results_binpair)
+    }
+
+    results <- matrix(data=0,nrow=binpairNum,ncol=5, dimnames=list(row.names(counts),c("bin1","bin2","stat","pvalue","pvalue.adj")))
+    results <- as.data.frame(results)
+    for(i in 1:binpairNum){
+        cat("\r",paste0("scHiCDiff.CVM is analyzing",i,"of",binpairNum,"bin paris"))
+        results[i,1:2] <- binpair[i,]
+        results[i,3:5] <- callDCI.CVM(i)
+    }
+
+    results[,"pvalue.adj"] <- p.adjust(results[,"pvalue"],method="fdr")
+    results <- results[order(results[,"stat"],decreasing = TRUE),]
+
+    return(results)
+}
